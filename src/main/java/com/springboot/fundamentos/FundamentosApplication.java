0a127a5d424cb3bfd2003c6cb468d2e430197760
Samuel Ricardo Quintero Agudelo <squinter@bancolombia.com.co>
2023-05-19 01:17:41 -0500

    Creating components and beans

--- src/main/java/com/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/springboot/fundamentos/FundamentosApplication.java
@@ -1,5 +1,9 @@
 package com.springboot.fundamentos;
 
+import com.springboot.fundamentos.bean.IMyBean;
+import com.springboot.fundamentos.bean.IMyBeanWithDependency;
+import com.springboot.fundamentos.component.IComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+    private final IComponentDependency componentDependency;
+    private final IMyBean myBean;
+    private final IMyBeanWithDependency myBeanWithDependency;
+
+    public FundamentosApplication(@Qualifier("componentAnotherImpl") IComponentDependency componentDependency, IMyBean myBean, IMyBeanWithDependency myBeanWithDependency) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FundamentosApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        componentDependency.greet();
+        myBean.print();
+        myBeanWithDependency.printWithDependency();
     }
 }
