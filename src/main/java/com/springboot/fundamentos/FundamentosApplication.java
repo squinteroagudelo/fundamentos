ce51a81f7a4ceb73276adfdcfe9f2b36ddfb84f9
Samuel Ricardo Quintero Agudelo <squinter@bancolombia.com.co>
2023-05-20 23:11:42 -0500

    Using logs - Logs level

--- src/main/java/com/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.springboot.fundamentos.bean.IMyBean;
 import com.springboot.fundamentos.bean.IMyBeanWithDependency;
 import com.springboot.fundamentos.component.IComponentDependency;
 import com.springboot.fundamentos.pojo.UserPojo;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,10 +15,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+    Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
+
     private final IComponentDependency componentDependency;
     private final IMyBean myBean;
     private final IMyBeanWithDependency myBeanWithDependency;
-    private IBeanWithProperties beanWithProperties;
+    private final IBeanWithProperties beanWithProperties;
     private final UserPojo userPojo;
 
     public FundamentosApplication(@Qualifier("componentAnotherImpl") IComponentDependency componentDependency, IMyBean myBean, IMyBeanWithDependency myBeanWithDependency, IBeanWithProperties beanWithProperties, UserPojo userPojo) {
@@ -38,5 +42,10 @@ public class FundamentosApplication implements CommandLineRunner {
         myBeanWithDependency.printWithDependency();
         System.out.println(beanWithProperties.function());
         System.out.printf("User info%nemail: %s%nPassword: %s%nAge: %d%n", userPojo.getEmail(), userPojo.getPassword(), userPojo.getAge());
+        try {
+            int value = 10/0;
+        } catch (Exception e) {
+            LOGGER.error("This is message error - Divide by zero: " + e.getMessage());
+        }
     }
 }
